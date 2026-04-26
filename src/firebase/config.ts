0652fd2a87eee0be018df48f80618fb6028317e4
Saksham Bhor <sakshambhor@gmail.com>
2026-04-26 14:46:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9741197854-fd9d5",
+  "appId": "1:811648795682:web:d83b987b129dbe6fd5367b",
+  "apiKey": "AIzaSyDn1K342aUU0seepc5bAkzrtYHAMu-EIZk",
+  "authDomain": "studio-9741197854-fd9d5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "811648795682"
+};
